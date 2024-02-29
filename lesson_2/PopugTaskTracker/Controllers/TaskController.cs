de5ce5b2030ab3f04498f57ce4c783a8bb4c37e3
Юрий Батраков <y.batrakov@mxmgt.com>
2024-02-29 10:22:42 +0300

    Добавил tasktracker сервис

---
+++ lesson_2/PopugTaskTracker/Controllers/TaskController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PopugTaskTracker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        public TaskController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Task> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Task
+            {
+                TaskId = rng.Next(-20, 55),
+                Description = $"Do this: {rng.Next(-20, 55)}"
+            });
+        }
+    }
+}
---
+++ lesson_2/PopugTaskTracker/Task.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PopugTaskTracker
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Description { get; set; }        
+    }
+}
