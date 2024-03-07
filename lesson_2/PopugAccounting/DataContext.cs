d467dc40688a28cda55e5634755f2771d49a727a
Юрий Батраков <y.batrakov@mxmgt.com>
2024-03-07 12:02:55 +0300

    Добавил endpoint-ы для аналитики

--- lesson_2/PopugAccounting/DataContext.cs
+++ lesson_2/PopugAccounting/DataContext.cs
@@ -3,12 +3,6 @@ using System;
 
 namespace PopugAccounting
 {
-    public static class TransactionType
-    {
-        public const string Assign = "Assign";
-        public const string Complete = "Complete";
-        public const string Payment = "Payment";
-    }
 
     public class BalanceTransactionDb
     {
